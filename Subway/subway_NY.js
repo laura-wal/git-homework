a2a5edee307abd4ef25d4f84b16a68661a102f45
Laura Walpole <l.a.walpole@googlemail.com>
2015-05-07 19:09:35 +0100

    code working

---
+++ Subway/subway_NY.js
@@ -0,0 +1,106 @@
+//Subway Intersect Assignment
+// Task: Take the line and stop that a user is getting on at and the line and stop that user is getting off at and prints the total number of stops for the trip. 
+
+// 1.Make a object fucntion for all stations, then make an array with all the stopes for one line first
+//2.Write an alert to ask for the stop and start stations - hold this information (var)
+//3. Add the index's toegther for the inputted journey - 'return' this information
+//3. add in arrays for the other lines.
+//3. count all the stops until US, then count all the stops after it? how to do this? loop? (maybe nest an array in an array). 
+//.Also: looki into intersections, fucntion sheets on Mod.
+
+// Object constuctor function, for all stataions.
+
+$(document).ready(function() { 
+
+
+
+function SubwayLines(stnNameArr) {
+this.stnNameArr = stnNameArr;
+}
+
+function findIntersection(allLines){
+    // loop over first of arrays
+    // check if the item exists in both other arrays,
+    var firstLine = allLines[0];
+    for(var i = 0; i < firstLine.length; i++){
+        if ( allLines[1].indexOf(firstLine[i]) >= 0 && allLines[2].indexOf(firstLine[i]) >= 0 ){
+            return firstLine[i];
+        }
+    }
+}
+
+// Create key for lines and array of stops
+var greyLine = new SubwayLines(['G8th', 'G6th', 'US', 'G3rd', 'G1st' ]);
+var yellowLine = new SubwayLines(['YTS', 'Y34th', 'Y28th', 'Y23rd', "US", "Y8th"]);
+var greenLine = new SubwayLines(['GNGC', 'GN33rd', 'GN28th', 'GN23rd', 'US', "GNAP"]);
+
+var startData = ""
+var endData = ""
+var allLines = [greyLine.stnNameArr, yellowLine.stnNameArr, greenLine.stnNameArr];
+var intersectionName = findIntersection(allLines); // "Union Square"
+
+// greenLine // => { stNameArr: [ ... ] }
+// greenLine.stNameArr.indexOf("US")
+
+// An alert that asks for you position
+//var start = prompt('Where do you want to leave from?');
+ $("#journStart").on('click', function(){
+       startData = $('#start-station').val();
+      console.log(startData);
+ })
+
+// Afind out where we are going
+//var end = prompt('Where do you want to go?');
+$("#journEnd").on('click', function(){
+       endData = $('#end-station').val();
+      console.log(endData);
+})
+
+// $("button").on('click', function(){
+//        var StartCal = var journeyCalc;
+//       console.log(StartData);
+// })
+
+// // tell it to start counting from the index
+// var leaveIndex = greyLine.stnNameArr.indexOf(leave);
+
+// // fwhere do we stop counting
+// var landIndex = greyLine.stnNameArr.indexOf(land);
+
+
+// if you are in the same line -> diff the indexes
+
+// otherwise do ... (whatever is in else)
+
+// var distance;
+ $("#journCalc").on('click', function(){
+    for(var i = 0; i < allLines.length; i++){
+        if ( allLines[i].indexOf(startData) >= 0 && allLines[i].indexOf(endData) >= 0 ){
+            var distance = Math.abs( allLines[i].indexOf(startData) - allLines[i].indexOf(endData) );
+        }    
+    }
+
+    if(!distance){
+        var startIndex, endIndex, startFromUS, endFromUS;
+        for(var i = 0; i < allLines.length; i++){
+            
+            if(allLines[i].indexOf(startData) >= 0){
+                startIndex = allLines[i].indexOf(startData);
+                // (index of start) - (index of US)
+                startFromUS = startIndex - allLines[i].indexOf(intersectionName); // indexOf("US"); 
+                startFromUS = Math.abs(startFromUS); 
+            }
+            if(allLines[i].indexOf(endData) >= 0){
+                endIndex = allLines[i].indexOf(end);
+                endFromUS = Math.abs( endIndex - allLines[i].indexOf(intersectionName) ); 
+            }
+        }
+        distance = startFromUS + endFromUS;
+    }
+
+    console.log(distance);
+
+});
+
+
+});
